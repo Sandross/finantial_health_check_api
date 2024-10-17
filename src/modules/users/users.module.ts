033973e75b7d5c99bccd17cf4106d5542ae86e40
Sandross <sandrosalasfilho3@gmail.com>
2024-10-17 08:06:14 -0300

    feat: creating repository

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { PrismaService } from 'src/database/prisma.service';
+import { UserRepository } from 'src/shared/database/repositories/user.repositories';
+import { PrismaService } from 'src/shared/database/prisma.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, PrismaService]
+  providers: [UsersService, UserRepository, PrismaService]
 })
 export class UsersModule {}
